52c72c96f1be3dcbc3c7d74f9c5c8dc26a797ea9
AhmadFadhelP <absedon17@gmail.com>
2025-05-24 22:15:02 +0800

    updated this main.dart and connecting firebase options

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart'; // Pastikan file ini ada di folder lib/
 import 'screen/selamat_datang.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
